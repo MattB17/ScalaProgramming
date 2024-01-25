3c850da4ab117cddea7fbcb6f683b217c6d731ce
MattB17 <matt.buckley1755@gmail.com>
2024-01-24 20:10:40 -0500

    Adding starter code for recursion assignment

---
+++ Recursion/main/scala/recfun/RecFun.scala
@@ -0,0 +1,28 @@
+package recfun
+
+object RecFun extends RecFunInterface {
+
+  def main(args: Array[String]): Unit = {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(s"${pascal(col, row)} ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = ???
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
