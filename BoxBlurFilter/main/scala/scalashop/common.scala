57fc272343edc714578133cb7177e96f82e0c6df
MattB17 <matt.buckley1755@gmail.com>
2024-06-08 08:14:26 -0400

    Adding boxBlurKernel

--- BoxBlurFilter/main/scala/scalashop/common.scala
+++ BoxBlurFilter/main/scala/scalashop/common.scala
@@ -6,22 +6,34 @@ import scala.util.DynamicVariable
 import org.scalameter.*
 
 /** The value of every pixel is represented as a 32 bit integer. */
+/** So an RGBA value is an integer of the form <r><g><b><a> where
+ * <r> is the most significant 8 bits representing the red component
+ * <g> is the next 8 bits representing the green component
+ * <b> is the following 8 bits for the blue component
+ * <a> is the last 8 bits for the transparent component */
 type RGBA = Int
 
 /** Returns the red component. */
+/** This extracts the most significant 8 bits */
 def red(c: RGBA): Int = (0xff000000 & c) >>> 24
 
 /** Returns the green component. */
+/** Truncates the least significant 16 bits and gets the next 8 bits */
 def green(c: RGBA): Int = (0x00ff0000 & c) >>> 16
 
 /** Returns the blue component. */
+/** Truncates the least significant 8 bits and gets the next 8 bits */
 def blue(c: RGBA): Int = (0x0000ff00 & c) >>> 8
 
 /** Returns the alpha component. */
+/** Gets the least significant 8 bits */
 def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
 
 /** Used to create an RGBA value from separate components. */
 def rgba(r: Int, g: Int, b: Int, a: Int): RGBA =
+  // Puts r as the most significant 8 bits, g as the next 8 bits,
+  // followed by 8 bits representing b, and lastly the least significant
+  // 8 digits are a
   (r << 24) | (g << 16) | (b << 8) | (a << 0)
 
 /** Restricts the integer into the specified range. */
@@ -37,10 +49,33 @@ class Img(val width: Int, val height: Int, private val data: Array[RGBA]):
   def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
 
 /** Computes the blurred RGBA value of a single pixel of the input image. */
-def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA =
+def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+  val srcWidth = src.width
+  val srcHeight = src.height
 
-  // TODO implement using while loops
-  ???
+  var redSum, greenSum, blueSum, alphaSum, pxCount = 0L
+  var currX = x - radius
+  var currY = y - radius
+
+  while (currX <= x + radius) {
+    while (currY <= y + radius) {
+      val currPx = src(clamp(currX, 0, srcWidth), clamp(currY, 0, srcHeight))
+      redSum += red(currPx)
+      greenSum += green(currPx)
+      blueSum += blue(currPx)
+      alphaSum += alpha(currPx)
+      pxCount += 1
+      currY += 1
+    }
+    currX += 1
+    currY = y - radius
+  }
+  rgba(
+    (redSum / pxCount).toInt,
+    (greenSum / pxCount).toInt,
+    (blueSum / pxCount).toInt,
+    (alphaSum / pxCount).toInt)
+}
 
 val forkJoinPool = ForkJoinPool()
 
--- BoxBlurFilter/test/scala/scalashop/BlurSuite.scala
+++ BoxBlurFilter/test/scala/scalashop/BlurSuite.scala
@@ -4,5 +4,30 @@ import java.util.concurrent.*
 import scala.collection.*
 
 class BlurSuite extends munit.FunSuite:
-  // Put tests here
-  ()
+  test("boxBlurKernel should correctly handle radius 0") {
+    val src = new Img(7, 7)
+
+    for (x <- 0 until 7; y <- 0 until 7)
+      src(x, y) = rgba(x + 5, y * 2, x * y, (x * x) + (y * y))
+
+    for (x <- 0 until 7; y <- 0 until 7)
+      assert(boxBlurKernel(src, x, y, 0) == src(x, y))
+  }
+
+  test("boxBlurKernel with radius 1") {
+    val src = new Img(3, 3)
+    src(0, 0) = 0
+    src(0, 1) = 1
+    src(0, 2) = 2
+    src(1, 0) = 4
+    src(1, 1) = 5
+    src(1, 2) = 8
+    src(2, 0) = 10
+    src(2, 1) = 10
+    src(2, 2) = 50
+
+
+    assert(boxBlurKernel(src, 1, 1, 1) == 10)
+    assert(boxBlurKernel(src, 0, 1, 1) == 2)
+    assert(boxBlurKernel(src, 1, 0, 1) == 4)
+  }
